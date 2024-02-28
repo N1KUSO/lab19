a912c23032d5a73a113b759c87bfb36aa44bc2a5
N1KUSO <kisilnikolay1356@gmail.com>
2024-02-28 11:37:30 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -2,6 +2,8 @@
 #define INC_VECTOR_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
 
 typedef struct vector {
     int *data;
@@ -10,6 +12,55 @@ typedef struct vector {
 } vector;
 
 vector createVector(size_t n) {
-    
+    vector vec;
+    vec.data = (int *) malloc(n * sizeof(int));
+
+    if (!vec.data) {
+        fprintf(stderr, "bad alloc\n");
+        exit(1);
+    }
+
+    vec.size = 0;
+    vec.capacity = n;
+    return vec;
 }
+
+void reserve(vector *vec, size_t newCapacity) {
+    if (newCapacity == 0) {
+        free(vec->data);
+        vec->data = NULL;
+        vec->capacity = 0;
+        vec->size = 0;
+    } else if (newCapacity < vec->size) {
+        vec->capacity = vec->size = newCapacity;
+        vec->data = (int *) realloc(vec->data, newCapacity * sizeof(int));
+        if (!vec->data) {
+            fprintf(stderr, "bad alloc\n");
+            exit(1);
+        }
+    } else {
+        vec->capacity = newCapacity;
+        vec->data = (int *) realloc(vec->data, newCapacity * sizeof(int));
+        if (!vec->data) {
+            fprintf(stderr, "bad alloc\n");
+            exit(1);
+        }
+    }
+}
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    v->data = (int*)realloc(v->data, v->size * sizeof(int));
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+    v->data = NULL;
+    v->size = 0;
+    v->capacity = 0;
+}
+
 #endif
