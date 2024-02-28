fa005e019a4f8d98eb24d8dfffd63801dc0627e5
N1KUSO <kisilnikolay1356@gmail.com>
2024-02-28 11:21:13 +0300

    vector structure

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,6 +1,15 @@
 #ifndef INC_VECTOR_H
 #define INC_VECTOR_H
 
+#include <stdio.h>
 
+typedef struct vector {
+    int *data;
+    size_t size;
+    size_t capacity;
+} vector;
 
+vector createVector(size_t n) {
+    
+}
 #endif
