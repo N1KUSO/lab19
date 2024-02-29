0aa96bce6a14246ff888e8ab8eb260ac466bf57e
N1KUSO <kisilnikolay1356@gmail.com>
2024-02-29 12:09:23 +0300

    memory usage of vectorvoid

--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 #include <assert.h>
 
-typedef struct vector {
+typedef struct vectorVoid {
     int *data;
     size_t size;
     size_t capacity;
@@ -15,5 +15,54 @@ typedef struct vector {
 
 } vectorVoid;
 
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    vectorVoid v;
+    v.data = malloc(n * baseTypeSize);
+
+    if (!v.data) {
+        fprintf(stderr, "bad alloc\n");
+        exit(1);
+    }
+
+    v.size = 0;
+    v.capacity = n;
+    v.baseTypeSize = baseTypeSize;
+    return v;
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    if (newCapacity > v->capacity) {
+        void *newData = realloc(v->data, newCapacity * v->baseTypeSize);
+        if (newData == NULL) {
+            fprintf(stderr, "bad alloc\n");
+            exit(1);
+        }
+        v->data = newData;
+        v->capacity = newCapacity;
+    }
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    if (v->size < v->capacity) {
+        void *newData = realloc(v->data, v->size * v->baseTypeSize);
+        if (newData == NULL) {
+            fprintf(stderr, "bad alloc\n");
+            exit(1);
+        }
+        v->data = newData;
+        v->capacity = v->size;
+    }
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+    v->data = NULL;
+    v->size = 0;
+    v->capacity = 0;
+}
 
 #endif
